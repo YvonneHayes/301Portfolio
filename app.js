7b87c91b5bdff1c885626f1c83c579e18d40d2bc
Yvonne Hayes <queyvonne@gmail.com>
2016-04-11 16:33:21 -0700

    starting out with basic html and constructor function

---
+++ app.js
@@ -0,0 +1,8 @@
+//Project creator function
+function Article (projectIndex) {
+    this.name = projectIndex.name;
+    this.summary = projectIndex.summary;
+    this.creator = projectIndex.creator;
+    this.collaborators = projectIndex.collaborators;
+    this.finishedOn = projectIndex.finishedOn
+}
---
+++ projects.js
@@ -0,0 +1,17 @@
+var projectsData = [
+  {
+    name:       'Bacon Ipsum',
+    summary:    '<p>food</p>',
+    creator:      'Kevin Bacon',
+    collaborators:   'http://www.imdb.com/name/nm0000102/',
+    finishedOn: '2015-11-05',
+  },
+  {
+    name:       'Bacon Ipsum',
+    summary:    '<p>food</p>',
+    creator:      'Kevin Bacon',
+    collaborators:   'http://www.imdb.com/name/nm0000102/',
+    finishedOn: '2015-11-05',
+  },
+
+];
