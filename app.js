8bb63c9f146ade6bf3d5c840014999c56bdd993a
Yvonne Hayes <queyvonne@gmail.com>
2016-04-11 19:11:12 -0700

    not working. No clue why not

--- app.js
+++ app.js
@@ -1,8 +1,50 @@
+
+var projects = [];
+
 //Project creator function
-function Article (projectIndex) {
+function Project (projectIndex) {
+    this.category = projectIndex.category;
     this.name = projectIndex.name;
     this.summary = projectIndex.summary;
     this.creator = projectIndex.creator;
     this.collaborators = projectIndex.collaborators;
-    this.finishedOn = projectIndex.finishedOn
+    this.finishedOn = projectIndex.finishedOn;
+    this.locationUrl = projectIndex.locationUrl
 }
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('div.template').clone();
+
+  $newProject.attr('data-category', this.category);
+  $newProject.find('#forProject').text(this.name);
+  $newProject.find('.projectSummary').text(this.summary);
+  $newProject.find('#creatorName').text(this.creator);
+  $newProject.find('#others').text(this.collaborators);
+  $newProject.find('pubdate').text(this.finishedOn);
+  $newProject.find('adress>href').text(this.locationUrl);
+
+  // Include the publication date as a 'title' attribute to show on hover:
+  $newProject.find('time[pubdate]').attr('title', this.finishedOn)
+
+  // Display the date as a relative number of "days ago":
+  $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.finishedOn))/60/60/24/1000) + ' days ago')
+
+  $newProject.append('<hr>');
+
+  $newProject.removeClass('template');
+
+  return $newProject;
+}
+
+
+// projectsData.sort(function(a,b) {
+//   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+// });
+//
+// projectsData.forEach(function(ele) {
+//   projects.push(new Project(ele));
+// })
+//
+// projects.forEach(function(a){
+//   $('#articles').append(a.toHtml())
+// });
--- projects.js
+++ projects.js
@@ -1,17 +1,20 @@
 var projectsData = [
   {
-    name:       'Bacon Ipsum',
-    summary:    '<p>food</p>',
-    creator:      'Kevin Bacon',
-    collaborators:   'http://www.imdb.com/name/nm0000102/',
-    finishedOn: '2015-11-05',
+    category: '201 Project',
+    name:       'Pizza 3000',
+    summary:    '<p>A webpage for a Pizza restaurant that builds tables via JS and dynamically updates sales numbers for various locations</p>',
+    creator:      'Yvonne Hayes',
+    collaborators:   'none',
+    finishedOn: '2016-03-20',
+    locationUrl: 'https://github.com/YvonneHayes/201Week2_Pizza',
   },
   {
-    name:       'Bacon Ipsum',
-    summary:    '<p>food</p>',
-    creator:      'Kevin Bacon',
-    collaborators:   'http://www.imdb.com/name/nm0000102/',
-    finishedOn: '2015-11-05',
+    category: '201 Project',
+    name:       'BusMall',
+    summary:    '<p>A webpag with a professional looking landing page and a cartoon/fun themed voting page, that counts clicks on a certain image and displays results in charts made with canvas and charts.js</p>',
+    creator:      'Yvonne Hayes',
+    collaborators:   'none',
+    finishedOn: '2016-03-28',
+    locationUrl: 'https://github.com/YvonneHayes/201Week3_BusMall',
   },
-
 ];
