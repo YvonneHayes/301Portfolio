c5ef1c3da0b7edfa2502d243c11e309beed3e310
Yvonne Hayes <queyvonne@gmail.com>
2016-04-18 16:30:44 -0700

    broke the home view

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var projectView = {};
-var projects = [];
+
+
 
 //Project creator function
 function Project (projectIndex) {
@@ -11,6 +11,8 @@ function Project (projectIndex) {
   this.finishedOn = projectIndex.finishedOn;
   this.locationUrl = projectIndex.locationUrl;
 }
+
+Project.all = [];
 
 Project.prototype.toHtml = function() {
 
@@ -25,94 +27,64 @@ Project.prototype.toHtml = function() {
 
 };
 
-
-
-projectsData.sort(function(a,b) {
-  return (new Date(b.finishedOn)) - (new Date(a.finishedOn));
-});
-
-projectsData.forEach(function(ele) {
-  projects.push(new Project(ele));
-});
-
-projects.forEach(function(a){
-  $('#projects').append(a.toHtml());
-});
-
-//********************************************************//
-//                    FILTERS                             //
-//*******************************************************//
-
-//populate Filter with categories
-projectView.populateFilters = function() {
-  var appTemplate = $('#selector-template').html();
-  var compileTemplate = Handlebars.compile(appTemplate);
-  $('.newProject').each(function() {
-    // var val = {
-    //   data: $(this).attr('data-category')
-    // };
-    // var optionTag = compileTemplate(val);
-    // $('#category-filter').append(optionTag);
-
-    val = {
-      data: $(this).attr('data-category')
-    };
-    optionTag = compileTemplate(val);
-    if ($('#category-filter option[value="' + val.data + '"]').length === 0) {
-      $('#category-filter').append(optionTag);
-    }
-  });
-};
-
-
-//Show only projects of the selected category (or all if blank)
-projectView.handleCategoryFilter = function() {
-  $('#category-filter').on('change', function() {
-    var categoryName = $(this).val(); //Turned value of category into a variable
-    if (categoryName) {
-
-      $('.newProject').hide(); //hiding ALL articles
-      $('.newProject[data-category="' + categoryName +'"]').fadeIn('slow'); //fade in JUST the one category
-
-    } else {
-      $('.template:not(.template)').fadeIn('fast'); //showing all projects but the template
-
-    }
-  });
-};
-
-
-
-//********************************************************//
-//                  VIEW OPTIONS                          //
-//*******************************************************//
-
-// Event Handler that turns Home and About into Tabs
-projectView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function(){
-    var dataContent = $(this).attr('data-content');
-    $('.tab-content').hide();
-    $('#' + dataContent).show();
+// Sorting
+Project.loadAll = function(projectsData) {
+  projectsData.sort(function(a,b) {
+    return (new Date(b.finishedOn)) - (new Date(a.finishedOn));
   });
 
-  $('.main-nav .tab:first').click();
-};
-
-//Set the teasers and read more link
-projectView.setTeasers = function() {
-  $('.projectSummary *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any artcile body.
-  $('.read-on').on('click', function(e){
-    e.preventDefault();
-    $(this).siblings('.projectSummary').find('*:nth-of-type(n+2)').show();
+  projectsData.forEach(function(ele) {
+    Project.all.push(new Article(ele));
   });
 
 };
 
-//Calling all functions as soon as ready
-$(document).ready(function(){
-  projectView.populateFilters();
-  projectView.handleCategoryFilter();
-  projectView.handleMainNav();
-  projectView.setTeasers();
 
-});
+// projectsData.sort(function(a,b) {
+//   return (new Date(b.finishedOn)) - (new Date(a.finishedOn));
+// });
+//
+// projectsData.forEach(function(ele) {
+//   projects.push(new Project(ele));
+// });
+//
+// projects.forEach(function(a){
+//   $('#projects').append(a.toHtml());
+// });
+
+
+Project.fetchAll = function() {
+  // if (localStorage.projectsData) {
+  //   // When rawData is already in localStorage,
+  //   // we can load it by calling the .loadAll function,
+  //   // and then render the index page (using the proper method on the articleView object).
+  //   Project.loadAll(JSON.parse(localStorage.rawData));
+  //   projectView.callAll(); //DONE: Change this fake method call to the correct one that will render the index page.
+  // } else {
+    // TODO: When we don't already have the rawData in local storage, we need to get it from the JSON file,
+    //       which simulates data on a remote server. Run live-server or pushstate-server!
+    //       Please do NOT browse to your HTML file(s) using a "file:///" link. RUN A SERVER INSTEAD!!
+
+    // 1. Retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
+    var ajaxCall = $.getJSON('projects.json', function(data){
+      console.log('hi there');
+    });
+    console.log(ajaxCall);
+
+
+    ajaxCall.done(function (data){
+      // 2. Store the resulting JSON data with the .loadAll method,
+      Project.loadAll(data);
+      console.log();
+      // 3. Cache the data in localStorage so next time we won't enter this "else" block (avoids hitting the server),
+      localStorage.projectsData = JSON.stringify(data);
+      // 4. Render the index page (perhaps with an articleView method?).
+
+
+
+
+    });
+  // }
+};
+Project.fetchAll();
+callAllNow();
