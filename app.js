ed8f1d84996307812b0e20c1529adca10e227b03
Yvonne Hayes <queyvonne@gmail.com>
2016-04-21 14:10:45 -0700

    added js code for frst HOF and with it created interests on About page. Implemented from sandboxing. Also did some styling so it looks just like before

--- app.js
+++ app.js
@@ -160,6 +160,25 @@
   //              ABOUT PAGE - INTERESTS                   //
   //*******************************************************//
 
+  function useNext(next) {
+    console.log('Here\'s a few things I like doing in my free time:');
+    document.getElementById('interests').innerHTML = 'Here\'s a few things I like doing in my free time:';
+
+    var likes = ['coding', 'gaming', 'reading'];
+    likes.forEach(function(el) { next(el);});
+  }
+
+  function hobbies(s) {
+    console.log('  I enjoy ' + s );
+    var node = document.createElement('li');
+    var textnode = document.createTextNode(s);
+    node.appendChild(textnode);
+    document.getElementById('hobbies').appendChild(node);
+  }
+
+  useNext(hobbies);
+
+
   //Calling all functions as soon as ready
   $(document).ready(function(){
     projectView.handleCategoryFilter();
