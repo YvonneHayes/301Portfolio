9dc681f11de693d845e5460205eb490b070f9708
Yvonne Hayes <queyvonne@gmail.com>
2016-04-26 14:25:26 -0700

    added github repos just like in the blog

---
+++ scripts/repo.js
@@ -0,0 +1,27 @@
+(function(module) {
+  var repos = {};
+
+  repos.all = [];
+
+  repos.requestRepos = function(callback) {
+    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
+    $.ajax ({
+      url: 'https://api.github.com/user/repos',
+      // data: {access_token: githubToken} // this is the parameter way, it's an alternative way
+      headers: {Authorization: 'token ' + githubToken } //this is the Header way. Both work.
+    }).done(function(data){
+      repos.all = data;
+      callback();
+    });
+  };
+
+  // DONE: Model method that filters the full collection for repos with a particular attribute.
+  // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
