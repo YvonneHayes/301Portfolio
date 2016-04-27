a34bd2d814244703710c71abf3a9e89d1b88603b
Yvonne Hayes <queyvonne@gmail.com>
2016-04-27 14:46:29 -0700

    changed url in repo.js

--- scripts/repo.js
+++ scripts/repo.js
@@ -4,19 +4,18 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
-    $.ajax ({
-      url: 'https://api.github.com/user/repos',
-      // data: {access_token: githubToken} // this is the parameter way, it's an alternative way
-      headers: {Authorization: 'token ' + githubToken } //this is the Header way. Both work.
-    }).done(function(data){
-      repos.all = data;
-      callback();
-    });
+    $.ajax({
+      url: '/github/user/repos',
+      method: 'GET',
+      success: function(data) {
+        repos.all = data;
+        repos.all = repos.with('description');
+        callback();
+      }
+    }
+  );
   };
 
-  // DONE: Model method that filters the full collection for repos with a particular attribute.
-  // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
   repos.with = function(attr) {
     return repos.all.filter(function(repo) {
       return repo[attr];
