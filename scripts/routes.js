b377f926903d5fed246e42eb2c428ad20d72a741
Yvonne Hayes <queyvonne@gmail.com>
2016-04-28 23:18:21 -0700

    will not render repos

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,25 +1,5 @@
-// (function(module){
-//   var routes = {};
-//
-//   routes.setRouteMappings = function() {
-//     page.base('/');
-//     page('/', projectController.index);
-//     page('about', aboutController.index);
-//
-//
-//     page();
-//   };
-//
-//   routes.setRouteMappings();
-//
-//   module.routes = routes; //making it global
-//
-// })(window);
 
-// DONE: Configure routes for this app with page.js, by registering each URL your app can handle,
-// linked to a a single controller function to handle it:
 page('/', projectController.index);
 page('/about', aboutController.index);
 
-// DONE: Activate page.js!
 page();
